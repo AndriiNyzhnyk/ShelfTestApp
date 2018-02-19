83a24091bf5b6fe5609871fe9cc1fc192f43de3b
Andrii Nyzhnyk <andrij1510@gmail.com>
2018-02-19 15:14:01 +0200

    add form on page

---
+++ public/js/main.js
@@ -0,0 +1,15 @@
+$(document).ready( () => {
+    $('form').submit( function(e) {
+            e.preventDefault();
+            let data = $(this).serialize();
+            let searchPlace = $('#place').val();
+            data.plase = searchPlace + '';
+            console.log(searchPlace);
+            console.log(data);
+            $.ajax({
+                    type: "POST",
+                    url: "/search",
+                    data: data
+            });
+        });
+});
--- server.js
+++ server.js
@@ -1,26 +1,33 @@
-const express = require("express");
+const express = require('express');
+let bodyParser = require('body-parser');
+let urlEncodedParser = bodyParser.urlencoded({extended: false});
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + '/public'));
 
-app.get("/", (req, res) => {
-    sendFile("/index.html", res);
+app.get('/', (req, res) => {
+    sendFile('/index.html', res);
+});
+
+app.post('/search', urlEncodedParser, (req, res) => {
+    if(!req.body) return res.sendStatus(400);
+    console.log(req.body);
 });
 
 // Обробник 404 помилки
 app.use((req, res, next) => {
     res.status(404);
-    res.type("text/plain");
-    res.send("404");
+    res.type('text/plain');
+    res.send('404');
 });
 
-// Обробник 505 помилки
+// Обробник 500 помилки
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500);
-    res.type("text/plain");
-    res.send("500");
+    res.type('text/plain');
+    res.send('500');
 });
 
 app.listen(app.get('port'), () => {
