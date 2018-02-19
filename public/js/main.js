55b5a2b2f974d7d957fb4092af1fa2f1a47752f9
Andrii Nyzhnyk <andrij1510@gmail.com>
2018-02-19 16:05:07 +0200

    add valid form

--- public/js/main.js
+++ public/js/main.js
@@ -2,14 +2,14 @@ $(document).ready( function() {
     $('#search').submit( function(e) {
             e.preventDefault();
             let data = $(this).serialize();
-            // let searchPlace = $('#place').val();
-            // data.plase = searchPlace + '';
-            // console.log(searchPlace);
-            // console.log(data);
+        if(data === {} ) {
+            alert('Заповніть всі поля ! !');
+        } else {
             $.ajax({
                     type: "POST",
                     url: "/search",
                     data: data
             });
-        });
+        }
+    });
 });
