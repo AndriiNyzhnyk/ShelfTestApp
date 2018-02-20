863512a6539ce82d7dcf95ddca05686c6d132c61
Andrii Nyzhnyk <andrij1510@gmail.com>
2018-02-20 11:35:33 +0200

    create file .csv

--- foursquare/index.js
+++ foursquare/index.js
@@ -1,5 +1,7 @@
 const request = require('request');
 const secret = require('./secret');
+const json2csv = require('json2csv');
+const fs = require('fs');
 
 function result(err, res, body) {
     if (err) {
@@ -12,13 +14,40 @@ function result(err, res, body) {
 function filterData(body) {
     if(!body) return console.log('Not Data');
 
+    let filterList = [];
     const data = JSON.parse(body);
+
     for(let i = 0; i < data.response.groups[0].items.length; i++) {
-        console.log(data.response.groups[0].items[i].venue.name);
-        console.log(data.response.groups[0].items[i].venue.location.city);
-        console.log(data.response.groups[0].items[i].venue.location.address);
-        console.log(data.response.groups[0].items[i].venue.location.lat, data.response.groups[0].items[i].venue.location.lng);
+        let name = data.response.groups[0].items[i].venue.name;
+        let city = data.response.groups[0].items[i].venue.location.city;
+        let address = data.response.groups[0].items[i].venue.location.address;
+        let lat = data.response.groups[0].items[i].venue.location.lat;
+        let lng = data.response.groups[0].items[i].venue.location.lng;
+
+        let item = {
+            name,
+            city,
+            address,
+            lat,
+            lng
+        };
+
+        filterList.push(item);
     }
+    createCsv(filterList);
+}
+
+function createCsv(list) {
+    let fields = ['name', 'city', 'address', 'lat', 'lng'];
+    let csv = json2csv({ data: list, fields: fields });
+
+    fs.writeFile("file.csv", "sep =,\n", (err) => {
+            if (err) throw err;
+        });
+    fs.appendFile('file.csv', csv, (err) => {
+            if (err) throw err;
+            console.log('file saved');
+        });
 }
 
 module.exports.setConfigQuery = (query, lat, lng, radius) => {
