07038ad32ef34c7618254777aed734855fa313c6
Andrii Nyzhnyk <andrij1510@gmail.com>
2018-02-20 11:12:56 +0200

    add function 'filterData'

--- foursquare/index.js
+++ foursquare/index.js
@@ -1,16 +1,23 @@
 const request = require('request');
-const fs = require('fs');
 const secret = require('./secret');
 
 function result(err, res, body) {
     if (err) {
         console.error(err);
     } else {
-        // console.log(body);
-        fs.writeFile("data.json", body, (error) => {
-            if(error) throw error;
-            console.log("recording is complete");
-        });
+        filterData(body);
+    }
+}
+
+function filterData(body) {
+    if(!body) return console.log('Not Data');
+
+    const data = JSON.parse(body);
+    for(let i = 0; i < data.response.groups[0].items.length; i++) {
+        console.log(data.response.groups[0].items[i].venue.name);
+        console.log(data.response.groups[0].items[i].venue.location.city);
+        console.log(data.response.groups[0].items[i].venue.location.address);
+        console.log(data.response.groups[0].items[i].venue.location.lat, data.response.groups[0].items[i].venue.location.lng);
     }
 }
 
@@ -23,9 +30,9 @@ module.exports.setConfigQuery = (query, lat, lng, radius) => {
                 client_secret: `${secret.clientSecret}`,
                 ll: `${lat},${lng}`,
                 query: `${query}`,
-                radius: `${radius}`,
+                radius: `${radius + 80000}`,
                 v: '20170801',
-                limit: 10
+                limit: 20
         }
     };
 
