029d1748c0fabadc7939bdb24f5bac53c7f651fb
Andrii Nyzhnyk <andrij1510@gmail.com>
2018-02-23 20:24:42 +0200

    add normalize.css-v8

--- server.js
+++ server.js
@@ -20,7 +20,6 @@ app.post('/search', urlEncodedParser, (req, res) => {
     if(!req.body) return res.sendStatus(400);
     // console.log(req.body);
     const body = req.body;
-    console.log(body);
     fourSquare.setConfigQuery(body.place, body.lat, body.lng, body.radius);
     res.send('ok');
 });
